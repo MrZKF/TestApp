7df253767923b7338599f5f2d7e67a46ee00ec40
MrZKF <18626255712@163.com>
2018-03-26 20:54:57 +0800

    添加GG类

---
+++ TestApp/GG.m
@@ -0,0 +1,13 @@
+//
+//  GG.m
+//  TestApp
+//
+//  Created by Mr.KF Mac Pro on 2018/3/26.
+//  Copyright © 2018年 Mr.KF Mac Pro. All rights reserved.
+//
+
+#import "GG.h"
+
+@implementation GG
+
+@end
